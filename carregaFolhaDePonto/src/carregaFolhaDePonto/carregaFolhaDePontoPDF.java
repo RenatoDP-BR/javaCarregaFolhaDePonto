5dd66136cb3369a64c7d2feb20b29c53418717e8
Renato dias Pereira <renatodp@hotmail.com>
2022-09-06 11:51:58 -0300

    Atualiza solucao para Ler a partir de CSV

--- carregaFolhaDePonto/src/carregaFolhaDePonto/carregaFolhaDePontoPDF.java
+++ carregaFolhaDePonto/src/carregaFolhaDePonto/carregaFolhaDePontoPDF.java
@@ -1,46 +1,33 @@
 package carregaFolhaDePonto;
 
-import java.io.File;
-import java.io.IOException;
-import org.apache.pdfbox.cos.COSDocument;
-import org.apache.pdfbox.io.RandomAccessBufferedFileInputStream;
-import org.apache.pdfbox.pdfparser.PDFParser;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.text.PDFTextStripper;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;	
 
 public class carregaFolhaDePontoPDF {
 
 	public static void main(String[] args) {
-		
-		File espelhoDePontoPDF = new File("C:\\Users\\renat\\Documents\\curso java nttdata\\Espelho de Ponto - Comp 08-22 - Ponto extraido em 16-08 - 08-23.PDF");
-		
-		try {
-			if (!espelhoDePontoPDF.exists()) {
-				System.out.println("Espelho de ponto não existe");
-			}
-            PDFParser parser = new PDFParser(new RandomAccessBufferedFileInputStream(espelhoDePontoPDF));
-            parser.parse();
-            COSDocument cosDoc = parser.getDocument();
-            PDFTextStripper pdfStripper = new PDFTextStripper();
-            PDDocument pdDoc = new PDDocument(cosDoc);
-            for (int i = 1; i <= pdDoc.getNumberOfPages(); i++) {
-                pdfStripper.setStartPage(i);
-                pdfStripper.setEndPage(i);
-                String parsedText = pdfStripper.getText(pdDoc);
-                interpretaPaginaPDFLida(parsedText);
-                System.out.println("Página " + i + ": " + parsedText);
-            }
-		}
-		catch (IOException e) {
-            e.printStackTrace();
-        }
-		finally {	
-		}		
+
+	    String arquivoCSV = "C:\\Users\\renat\\Documents\\curso java nttdata\\Espelho de Ponto por Período (2).csv";
+	    String linha = "";
+	    try {
+	    	BufferedReader bufferArquivo = new BufferedReader(new InputStreamReader(new FileInputStream(arquivoCSV), "ISO-8859-1"));
+	        while ((linha = bufferArquivo.readLine()) != null) {
+
+	            System.out.println("Linha Lida:" + linha);
+
+	        }
+
+	    } catch (FileNotFoundException e) {
+	        e.printStackTrace();
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
+	    
     }
-	
-	public static void interpretaPaginaPDFLida(String parsedText) {
-		
-	}
 	
 	
 }
