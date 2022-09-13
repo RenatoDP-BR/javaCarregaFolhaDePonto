4ff758890f29295c5a7f3797b5317c19f10d9ff7
Renato dias Pereira <renatodp@hotmail.com>
2022-09-13 13:48:27 -0300

    Correção de import

--- carregaFolhaDePonto/src/carregaFolhaDePonto/carregaFolhaDePontoPDF.java
+++ carregaFolhaDePonto/src/carregaFolhaDePonto/carregaFolhaDePontoPDF.java
@@ -3,10 +3,11 @@ package carregaFolhaDePonto;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.io.FileNotFoundException;
 import java.io.IOException;	
 
-import CsvLancamentos; /* nao estou sabendo como puxar a classe CsvLancamentos */
+import carregaFolhaDePonto.CsvLancamentos;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
@@ -21,26 +22,27 @@ public class carregaFolhaDePontoPDF {
         boolean jaObtiveMesLancamentos, 
     	        jaObtiveDadosFuncionario, 
     	        JaContabilizeiLancamentos;
-        int cntLinhaCartao, 
-            mesCartaoLancamentos,
-            anoCartaoLancamentos, 
+        int cntLinhaCartao = 0, 
+            mesCartaoLancamentos = 0,
+            anoCartaoLancamentos = 0, 
             idFuncionario = 0;
         
-	    try {
 	    	try (BufferedReader bufferArquivo = new BufferedReader(new InputStreamReader(new FileInputStream(arquivoCSV), "ISO-8859-1"))) {
 				while ((linha = bufferArquivo.readLine()) != null) {
 					
 					cntLinhaCartao++; 
 					
 			        int indexInicial = linha.indexOf("Cartão");
-			        int indexFinal = linha.indexOf("Ponto");
+			        int indexFinal = linha.indexOf("Ponto")+5;
 			        System.out.println("Linha " + cntLinhaCartao + " do Cartao de Ponto:" + linha);
-			        
-                    if (linha.substring(indexInicial, indexFinal - 1) != "Cartão de Ponto") {
+			        String variavel = linha.substring(indexInicial, indexFinal);
+                    if (variavel != "Cartão de Ponto") {
                         if (jaObtiveMesLancamentos = false &
                            cntLinhaCartao == 2) {
-                        	System.out.println("Mes do Cartao de Ponto:" + linha.substring(9, 10));
-                        	mesCartaoLancamentos = Integer.parseInt(linha.substring(9, 10));
+                        	System.out.println("Mes do Cartao de Ponto:" + linha.substring(12, 13));
+                        	System.out.println("Ano do Cartao de Ponto:" + linha.substring(15, 18));
+                        	mesCartaoLancamentos = Integer.parseInt(linha.substring(12, 13));
+                        	anoCartaoLancamentos = Integer.parseInt(linha.substring(15, 18));
                         	jaObtiveMesLancamentos = true;
                         	continue;
                         }
@@ -73,27 +75,25 @@ public class carregaFolhaDePontoPDF {
                     	JaContabilizeiLancamentos = false;
                     	cntLinhaCartao = 1; 
                     	continue; 
-                    }
-                    	
-                    } /* fim de While */
-
-				} /* fim do try do buffer */
-	    	
-			} /* fim do try inteiro */
+                    }                    	
+                }
+				} catch (UnsupportedEncodingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} 
+				} 
 	    
-
-	    } catch (FileNotFoundException e) {
-	        e.printStackTrace();
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	    }
-	    
-    }
 	
 private static void contabilizaLancamentos(String dadosLancamentos) {
 
-	/* o objetivo é transformar o dadosLancamentos (em formato csv) em CsvLancamento (tabulando ele em campos),
-	 * para ser mais facilmente contabilizado */
+	//* o objetivo é transformar o dadosLancamentos (em formato csv) em CsvLancamento (tabulando ele em campos),
+	// * para ser mais facilmente contabilizado */
 	
 	System.out.println(dadosLancamentos);
 }
@@ -101,8 +101,8 @@ private static void contabilizaLancamentos(String dadosLancamentos) {
 
 private static Integer ultimoDiaMesLancamento(Integer mesCartao, Integer anoCartao) {
 	
-	/* o objetivo é pegar o ultimo dia do mes/ano enviado... pra isso vou procurar pra ver se já tem alguma função 
-	 * que já me calcule isso, pois tem anos bissextos em que fevereiro tem 29 dias */
+	//* o objetivo é pegar o ultimo dia do mes/ano enviado... pra isso vou procurar pra ver se já tem alguma função 
+	// * que já me calcule isso, pois tem anos bissextos em que fevereiro tem 29 dias */
 	
 	return 31; 
 	
